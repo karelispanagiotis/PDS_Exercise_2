d99ecf97e0c956de28b3f01cd74c3cec68197ac1
kitsiosk <kitsiosk@ece.auth.gr>
2019-12-06 19:26:17 +0200

    Minimized tester

--- bench/bench/tester_mpi.c
+++ bench/bench/tester_mpi.c
@@ -153,6 +153,25 @@ void testMPI( int    const n,
 }
 
 
+
+
+void testMPI2(int n, int d, int k) {
+  int p, id;                    // MPI # processess and PID
+  //MPI_Status Stat;              // MPI status
+  // int dst, rcv, tag;            // MPI destination, receive, tag
+
+  
+  MPI_Comm_rank(MPI_COMM_WORLD, &id); // Task ID
+  MPI_Comm_size(MPI_COMM_WORLD, &p);  // # tasks
+  
+  int chunk_size = n/p; // wont work if n/p not integer
+  double *X = ralloc(chunk_size * d);
+
+  knnresult result = distrAllkNN(X, chunk_size, d, k);
+
+  free( X );
+}
+
 int main(int argc, char *argv[])
 {
 
@@ -170,7 +189,8 @@ int main(int argc, char *argv[])
   // ============================== RUN EXPERIMENTS
 
   time_t t = clock();
-  testMPI( n, d, k, COLMAJOR );
+  // testMPI( n, d, k, COLMAJOR );
+  testMPI2(n, d, k);
   t = clock() - t;
   double execTime = (double)t/CLOCKS_PER_SEC;
 
